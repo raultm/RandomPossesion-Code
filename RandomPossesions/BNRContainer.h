9bea4d703585aa4a8f348334ba82adcdee848189
Raul Tierno <raultm@gmail.com>
2012-06-23 15:22:58 +0200

    First approach Gold Challenge complete - Main

---
+++ RandomPossesions/BNRContainer.h
@@ -0,0 +1,13 @@
+//
+//  BNRContainer.h
+//  RandomPossesions
+//
+//  Created by Raul Tierno Magro on 23/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@interface BNRContainer : BNRItem
+
+@end
---
+++ RandomPossesions/BNRContainer.m
@@ -0,0 +1,13 @@
+//
+//  BNRContainer.m
+//  RandomPossesions
+//
+//  Created by Raul Tierno Magro on 23/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BNRContainer.h"
+
+@implementation BNRContainer
+
+@end
