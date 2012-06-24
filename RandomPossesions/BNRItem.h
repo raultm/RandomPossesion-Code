d08e0b18d119bd6c97babe345e2503bb5e338ee2
Raul Tierno <raultm@gmail.com>
2012-06-24 18:41:34 +0200

    Use of properties instead of accessors

--- RandomPossesions/BNRItem.h
+++ RandomPossesions/BNRItem.h
@@ -28,24 +28,11 @@
 - (id) initWithItemName:(NSString *)name
            serialNumber:(NSString *)sNumber;
 
-- (void)setContainedItem:(BNRItem *)i;
-- (BNRItem *)containedItem;
-
-- (void)setContainer:(BNRItem *)i;
-- (BNRItem *)container;
-
-
-- (void)setItemName:(NSString *)str;
-- (NSString *)itemName;
-
-- (void)setSerialNumber:(NSString *)str;
-- (NSString *)serialNumber;
-
-- (void)setValueInDollars:(int)i;
-- (int) valueInDollars;
-
-- (NSDate *)dateCreated;
-
-
+@property (nonatomic, strong) BNRItem *containedItem;
+@property (nonatomic, weak) BNRItem *container;
+@property (nonatomic, strong) NSString *itemName;
+@property (nonatomic, strong) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly, strong) NSDate *dateCreated;
 
 @end
